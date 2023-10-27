03384e913d4cc5846a8abc271b096ae855cf6d6e
axelbernard20 <axel.bernard@etudiant.univ-rennes.fr>
2023-10-27 11:18:23 +0200

    Associations

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { AssociationsController } from './associations/associations.controller';
+import { AssociationsService } from './associations/associations.service';
+import { AssociationsModule } from './associations/associations.module';
 
 @Module({
-  imports: [UsersModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UsersModule, AssociationsModule],
+  controllers: [AppController, AssociationsController],
+  providers: [AppService, AssociationsService],
 })
 export class AppModule {}
