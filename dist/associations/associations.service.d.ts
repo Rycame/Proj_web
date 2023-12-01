99be1c76b6985f063679a38ee3d551d5b8519fed
mathieu.cyr@laposte.net <mathieu.cyr@laposte.net>
2023-12-01 09:22:35 +0100

    Changement Ignore

--- dist/associations/associations.service.d.ts
+++ dist/associations/associations.service.d.ts
@@ -1,2 +1,14 @@
+import { UsersService } from '../users/users.service';
+import { Association } from './associations.entity';
+import { User } from '../users/user.entity';
 export declare class AssociationsService {
+    private service;
+    private associations;
+    constructor(service: UsersService);
+    create(idUsers: number[], name: string): Association;
+    getAll(): Association[];
+    getById(id: number): Association | undefined;
+    update(id: number, idUsers: number[], name: string): Association | undefined;
+    delete(id: number): boolean;
+    getMembers(associationId: number): User[];
 }
